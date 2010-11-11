47db83b95070e12a3d32003a371a4d3d57f41bdc
André Taiar <taiar@hotmail.com>
2010-11-11 10:58:10 -0200

    dando problema com entrada grande.

---
+++ src/testes/geratestes.php
@@ -0,0 +1,27 @@
+<?php
+
+$nCamadas = 5;
+$capCamInicial = 30000;
+$totalReq = 100000;
+$counterzao = 1;
+$politicas = array("lru", "mru", "lfu", "fifo");
+
+echo $nCamadas . " ";
+
+for($i = 0; $i < $nCamadas; $i += 1)
+  echo rand(10, 10000) . " " . rand(10, 1000) . " " . $politicas[rand(0,3)] . " ";
+
+echo $capCamInicial . "\n";
+
+echo $totalReq . "\n";
+
+for($i = 0; $i < $totalReq; $i += 1)
+{
+  echo rand(1,$totalReq) . " ";
+  if(rand(0,1) == 1)
+    $counterzao += 1;
+  echo $counterzao . "\n";
+
+}
+
+?>
