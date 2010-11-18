45ae4aec5c85dcc3d462d02b88f2eb30960e6870
André Taiar <taiar@hotmail.com>
2010-11-18 08:38:57 -0200

    definindo testes

--- src/testes/gera2n.php
+++ src/testes/gera2n.php
@@ -1,11 +1,11 @@
 <?php
 
-$n = 50;
+$n = 10000;
 $cont = 1;
 echo $n . "\n";
 for($i = 0; $i < $n; $i += 1)
 {
-  echo mt_rand(0,89) . " ";
+  echo mt_rand(0,1000) . " ";
   if(mt_rand()&1) $cont += 1;
   echo $cont . "\n";
 }
