129438378231a5170a726eb270f3ad3aa5b0f421
André Taiar <taiar@hotmail.com>
2010-11-22 20:57:18 -0200

    contagem de tempo para fazer a avaliacao

--- src/tp5.c
+++ src/tp5.c
@@ -7,6 +7,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 #include "camadas.h"
 #include "simulacao.h"
@@ -17,6 +19,18 @@ main(int argc, char **argv)
   int nCamadas; // número de camadas de cada hierarquia de memória
   Hierarquia memoria; // estrutura que contém a hierarquia de memória a ser simulada
   Simulacao simulacao; // estrutura que guarda os resultados da simulação
+
+  // Variáveis para medida de tempo de execução
+  float tempo_execucao;
+  float tempo_usuario;
+  float tempo_sistema;
+  struct timeval t_inicio;
+  struct timeval t_fim;
+  struct rusage uso_recursos;
+  struct timeval t_usuario;
+  struct timeval t_sistema;
+
+  gettimeofday(&t_inicio, NULL);
 
   int tFlag = scanf("%d", &nCamadas);
   while (tFlag > 0)
@@ -35,5 +49,23 @@ main(int argc, char **argv)
     // Lê se existem mais testes
     tFlag = scanf("%d", &nCamadas);
   }
+
+  // Cálculo dos tempos de execução
+  gettimeofday(&t_fim, NULL);
+  getrusage(RUSAGE_SELF, &uso_recursos);
+
+  t_usuario = uso_recursos.ru_utime;
+  t_sistema = uso_recursos.ru_stime;
+
+  tempo_usuario = (float) t_usuario.tv_sec + (float) t_usuario.tv_usec / 1000000;
+  tempo_sistema = (float) t_sistema.tv_sec + (float) t_sistema.tv_usec / 1000000;
+
+  tempo_execucao = (t_fim.tv_sec - t_inicio.tv_sec) * 1000000;
+  tempo_execucao = (tempo_execucao + (t_fim.tv_usec - t_inicio.tv_usec)) / 1000000;
+
+  printf("tempo de execucao: %f\n", tempo_execucao);
+  printf("tempo de usuario: %f\n", tempo_usuario);
+  printf("tempo de sistema: %f\n", tempo_sistema);
+
   return EXIT_SUCCESS;
 }
